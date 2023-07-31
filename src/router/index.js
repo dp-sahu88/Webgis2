272279ab91737a557c3d7657f8b20324a44da3e4
Deviprasad sahu <deviprasadsahu88@gmil.com>
2023-07-31 16:13:30 +0530

    created a new route for the 3d view

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import TwoDim from '../views/TwoDim.vue'
+import ThreeDim from '../views/ThreeDim.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: '2d',
+      component: TwoDim
+    },{
+      path: '/3d',
+      name: '3d',
+      component: ThreeDim
     }
     // ,
     // {
