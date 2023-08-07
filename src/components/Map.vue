07d45fa2a302170342eef98b7cf2430cbd688578
= <=>
2023-08-07 14:29:41 +0530

    moved the code of flyTo function to a new file in utils

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -114,6 +114,7 @@ import {GPX, GeoJSON, IGC, KML, TopoJSON } from "ol/format";
 import { Vector as VectorSource } from "ol/source";
 import {Vector as VectorLayer} from "ol/layer";
 import DragAndDrop from 'ol/interaction/DragAndDrop.js';
+import flyTo from '../utils/FlyTo2D.js'
 import { useLayerSources } from "../stores/SourceList";
 import axios from "axios";
 const center = ref([116.547539, 40.450996]);
@@ -167,8 +168,9 @@ function getData() {
             }
             let loc = newData[0].getGeometry().getExtent().slice(0,2)
             if (element.loaded == false){
+                let view = map.value.getView()
                 console.log(loc);
-                setTimeout(flyTo(loc, ()=>{}),5000)
+                setTimeout(flyTo(loc, view ,()=>{}),5000)
                 element.loaded=true
             }
         }).catch((reason)=>{console.warn(reason)});
@@ -177,41 +179,41 @@ function getData() {
 }
 getData();
 
-function flyTo(location, done) {
-  let view = map.value.getView()
-  const duration = 2000;
-  const zoom = view.getZoom();
-  let parts = 2;
-  let called = false;
-  function callback(complete) {
-    --parts;
-    if (called) {
-      return;
-    }
-    if (parts === 0 || !complete) {
-      called = true;
-      done(complete);
-    }
-  }
-  view.animate(
-    {
-      center: location,
-      duration: duration,
-    },
-    callback
-  );
-  view.animate(
-    {
-      zoom: zoom - 1,
-      duration: duration / 2,
-    },
-    {
-      zoom: zoom,
-      duration: duration / 2,
-    },
-    callback
-  );
-}
+// function flyTo(location, done) {
+//   let view = map.value.getView()
+//   const duration = 2000;
+//   const zoom = view.getZoom();
+//   let parts = 2;
+//   let called = false;
+//   function callback(complete) {
+//     --parts;
+//     if (called) {
+//       return;
+//     }
+//     if (parts === 0 || !complete) {
+//       called = true;
+//       done(complete);
+//     }
+//   }
+//   view.animate(
+//     {
+//       center: location,
+//       duration: duration,
+//     },
+//     callback
+//   );
+//   view.animate(
+//     {
+//       zoom: zoom - 1,
+//       duration: duration / 2,
+//     },
+//     {
+//       zoom: zoom,
+//       duration: duration / 2,
+//     },
+//     callback
+//   );
+// }
 
 // set interaction to get the data by drag and drop action
 function setInteraction() {
