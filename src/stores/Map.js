d12b35db7b8d0cd62076860710b06cefb764ad8e
= <=>
2023-08-14 16:59:30 +0530

    added a property to the Map Store to hold selected layer object

--- src/stores/Map.js
+++ src/stores/Map.js
@@ -5,11 +5,21 @@ export const useMap = defineStore('mapStore', () => {
   const map = ref({})  
   const view = ref({})
   const selectedLayer = ref('')
+  const selectedLayerObj = ref({})
   function addToSelectedLayer(layername){
       if (layername == undefined){
         return
+      }else if (selectedLayer.value == layername) {
+        selectedLayer.value = ''
+        selectedLayerObj.value = {}
+        return
       }
-      selectedLayer.value = layername
+      map.value.getLayers().forEach(function (layer) {
+        if (layer.get('name') != undefined && layer.get('name') === layername) {
+            selectedLayerObj.value = layer
+            selectedLayer.value = layername
+        }
+      });
   }
-  return { map, view , selectedLayer, addToSelectedLayer}
+  return { map, view, selectedLayer, selectedLayerObj, addToSelectedLayer}
 })
