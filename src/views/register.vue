3cb23f99158edb674e89cf05b5dd2660414edf8b
Deviprasad sahu <deviprasadsahu88@gmail.com>
2023-08-19 10:32:58 +0530

    added register view and updated the login component.

--- src/views/register.vue
+++ src/views/register.vue
@@ -44,9 +44,14 @@
         </div>
 
         <div
-            class="m-auto relative z-10 w-fit top-20 py-4 px-6 bg-slate-300/50 backdrop-blur-sm rounded-md border focus-within:bg-slate-300/60">
-            <h1 class="text-4xl font-bold text-center my-6 text-white">Login</h1>
-            <form @submit.prevent="login" class="flex flex-col ">
+            class="m-auto relative z-10 w-fit top-10 py-2 px-6 bg-slate-300/50 backdrop-blur-sm rounded-md border focus-within:bg-slate-300/60">
+            <h1 class="text-4xl font-bold text-center my-6 text-white">Sign up</h1>
+            <form @submit.prevent="register" class="flex flex-col ">
+                <div class="form-group mb-4">
+                    <label for="inputName" class="block text-white ">Name</label>
+                    <input type="text" class="rounded-full px-2 mt-1 py-1 md:w-[40ch] bg-blue-100 border-b-2" id="inputName"
+                        placeholder="Deadpool" v-model="name" required>
+                </div>
                 <div class="form-group mb-4">
                     <label for="inputEmail1" class="block text-white ">Email</label>
                     <input type="email" class="rounded-full px-2 mt-1 py-1 md:w-[40ch] bg-blue-100 border-b-2"
@@ -57,8 +62,13 @@
                     <input type="password" class="rounded-full px-2 mt-1 py-1  md:w-[40ch] bg-blue-100 border-b-2"
                         id="inputPassword1" placeholder="Password" v-model="password" required>
                 </div>
+                <div class="form-group mb-4">
+                    <label for="inputPassword2" class="block text-white ">Confirm password</label>
+                    <input type="password" class="rounded-full px-2 mt-1 py-1  md:w-[40ch] bg-blue-100 border-b-2"
+                        id="inputPassword2" placeholder="Confirm password" v-model="c_password" required>
+                </div>
                 <button type="submit"
-                    class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 my-3 ">Submit</button>
+                    class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 my-3 ">Sign up</button>
             </form>
         </div>
     </div>
@@ -66,21 +76,24 @@
 <script setup>
 import { ref } from 'vue';
 import axios from 'axios';
+const name = ref('');
 const email = ref('')
 const password = ref('')
-
-const login = () => {
-    let url = import.meta.env.VITE_API_URL + '/login';
+const c_password = ref('')
+const register = () => {
     let data = new FormData();
+    let url = import.meta.env.VITE_API_URL + '/register';
+    data.append('name', name.value);
     data.append('email', email.value);
     data.append('password', password.value);
+    data.append('c_password', c_password.value);
 
     let config = {
         method: 'post',
         maxBodyLength: Infinity,
         url: url,
         headers: {
-            'Accept': 'application/json'
+            'Accept':'application/json'
         },
         data: data
     };
