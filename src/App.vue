a9810cbcac38a74becb09932545d3e2287582e8b
Deviprasad sahu <deviprasadsahu88@gmail.com>
2023-08-21 17:44:50 +0530

    added the notifications component to the app

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import {  RouterView } from 'vue-router'
 import Header from './components/Header.vue';
+import Notifications from './components/Notification/Notifications.vue';
 </script>
 
 <template>
   <Header></Header>
+  <Notifications/>
   <RouterView />
 </template>
 
