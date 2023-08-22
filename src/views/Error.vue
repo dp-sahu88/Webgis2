4859e50d9b483930998def94c0d0d29b3b604f09
Deviprasad sahu <deviprasadsahu88@gmail.com>
2023-08-22 12:32:24 +0530

    added the error component

---
+++ src/views/Error.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="text-[10rem] bg-black h-[100vh] pt-[4rem] ">
+        <img :src=image :alt="ecode" class="m-auto">
+    </div>
+</template>
+<script setup>
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router'
+const route = useRoute()
+const ecode = route.params.ecode
+const image = ref('')
+onMounted(() => { 
+    https://httpstatusdogs.com/img/100.jpg
+    image.value = `https://httpstatusdogs.com/img/${ecode}.jpg`
+ })
+</script>
+<style scoped></style>
